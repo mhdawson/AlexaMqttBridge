ad1b343141524cd477ed7ac4f0c13124174d4fc4
Michael Dawson <mdawson@devrus.com>
2017-04-23 12:41:28 -0400

    src: remove spaces in device to make matching more consistent

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ const requestHandler = (request, response) => {
     // a default device entry
     var device = 'default';
     if ((intent.slots.Device) && (intent.slots.Device.value)) {
-      device = intent.slots.Device.value.toString().toLowerCase().replace("'", "");;
+      device = intent.slots.Device.value.toString().toLowerCase().replace("'", "").replace(" ", "");
     }
 
     if (intent && intent.name && config.intents[intent.name]) {
